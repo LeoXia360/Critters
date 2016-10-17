db4aadba4028cfb196594b4e37acf3338ccb2d6b
Leo Xia <Leoxia360@gmail.com>
2016-10-16 22:22:25 -0500

    displayWorld

--- src/assignment4/Critter.java
+++ src/assignment4/Critter.java
@@ -291,26 +291,25 @@ public abstract class Critter {
 		char[][] world = new char[Params.world_height + 2][Params.world_width + 2];
 		boolean printed = false;
 		for (int row = 0; row < Params.world_height + 2; row++){
-			
 			for (int col = 0; col < Params.world_width + 2; col++){
 				//if a corner
 				if((row == 0 && col == 0) ||
-				   (row == 0 && col == Params.world_width + 2) ||
-				   (col == 0 && row == Params.world_height + 2) ||
-				   (col == Params.world_width + 2 && row == Params.world_height + 2)){
+				   (row == 0 && col == Params.world_width + 1) ||
+				   (col == 0 && row == Params.world_height + 1) ||
+				   (col == Params.world_width + 1 && row == Params.world_height + 1)){
 					world[row][col] = '+';
 					System.out.print(world[row][col]);
 					printed = true;
 				}
 				
 				//print top or bottom border
-				if (!printed && (row == 0) || (row == Params.world_height + 2)){
+				if (!printed && ((row == 0) || (row == Params.world_height + 1))){
 					world[row][col] = '-';
 					System.out.print(world[row][col]);
 					printed = true;
 				}
 				//print left or right border
-				if(!printed && (row == 0) || (row == Params.world_width + 2)){
+				if(!printed && ((col == 0) || (col == Params.world_width + 1))){
 					world[row][col] = '|';
 					System.out.print(world[row][col]);
 					col++;
@@ -328,6 +327,7 @@ public abstract class Critter {
 				
 				if(!printed){
 					world[row][col] = ' ';
+					System.out.print(world[row][col]);
 					System.out.print(world[row][col]);
 				}
 				
