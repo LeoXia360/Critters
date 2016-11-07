76d609497f830a037ac04a7cbf340c3aaa758343
kevinwong94 <wong.kevin94@gmail.com>
2016-11-07 00:02:10 -0600

    Can read values from text field

--- src/assignment5/Main.java
+++ src/assignment5/Main.java
@@ -1,6 +1,13 @@
 package assignment5;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.ws.Response;
+
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
@@ -16,6 +23,7 @@ import javafx.animation.AnimationTimer;
 public class Main extends Application{
 
 	static GridPane grid = new GridPane();
+	//static GridPane grid2 = new GridPane();
 	static Timer time = new Timer();
 	static double animationSpeed = 1.0;
 	static boolean amRunning = false;
@@ -26,28 +34,10 @@ public class Main extends Application{
 		launch(args);
 	}
 	
-//	public void Reminder(){
-//		toolkit = Toolkit.getDefaultToolkit();
-//		time.schedule(new ScheduleTask(), (double)(1000/animationSpeed));
-//	}
-	
-	class ScheduleTask extends TimerTask{
-		public void run(){
-			System.out.println("working");
-			Critter.worldTimeStep();
-			Critter.displayWorld();
-			System.exit(0);
-		}
-	}
-	
 	@Override
 	public void start(Stage primaryStage) {
 		Scene scene = new Scene(grid, Params.world_width * Painter.size, Params.world_height * Painter.size);
-//		primaryStage.setScene(scene);
-//		primaryStage.setTitle("Grid");
-//		primaryStage.show();
-		// paints the icons
-        
+		
 		try {
 			for(int i = 0; i < 2; i++){
 	        	Critter.makeCritter("Craig");
@@ -62,24 +52,188 @@ public class Main extends Application{
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Grid");
 		primaryStage.show();
-		Critter.displayWorld();	        
-	    Critter.worldTimeStep();
-	    primaryStage.show();
-	    Critter.displayWorld();
-	    Stage secondStage = new Stage();
-		secondStage.setTitle("Second Stage");
 		
-		new AnimationTimer(){
-			@Override
-			public void handle(long now){
-				if (now%100 == 0){
-					Critter.displayWorld();
-					Critter.worldTimeStep();
-				}
-			}
-		}.start();
+//		new AnimationTimer(){
+//			@Override
+//			public void handle(long now){
+//				if (now%100 == 0){
+//					System.out.println(now);
+//					Critter.displayWorld();
+//					Critter.worldTimeStep();
+//				}
+//			}
+//		}.start();
+		
+//		GridPane grid2 = new GridPane();
+//		Stage secondStage = new Stage();
+//		secondStage.setTitle("Second Stage");
+//		grid2.setVgap(500);
+//		grid2.setHgap(1200);
+//		grid2.add(new Label("Make Critter: "), 0, 0);
 
-//		System.out.println("done");
+//		grid2.add(critterComboBox, 200, 0);
+//		grid2.add(new Label("Amount of Critters: "), 10000, 0);
+//		
+//		Scene secondScene = new Scene(grid2, 1000, 200);
+//		secondStage.setScene(secondScene);
+//		secondStage.setTitle("Second Stage");
+//		secondStage.show();
+		
+//		Button btn = new Button();
+//        btn.setText("Push Button to Increment Counter");
+//        btn.setOnAction(new EventHandler<ActionEvent>() {
+// 
+//            @Override
+//            public void handle(ActionEvent event) {
+//            	try{
+//            		Critter.makeCritter("Craig");
+//        			List<Critter> c = new ArrayList<Critter>();
+//        			c = Critter.getInstances("Craig");
+//        			Critter.runStats(c);
+//            	}catch (Exception e) {
+//        			e.printStackTrace();
+//        		}
+//            }
+//        });
+//        grid2.add(btn, 0, 500);
+//        primaryStage.setTitle("Hello World!");
+//        Button btn = new Button();
+//        btn.setText("Say 'Hello World'");
+//        btn.setOnAction(new EventHandler<ActionEvent>() {
+// 
+//            @Override
+//            public void handle(ActionEvent event) {
+//                System.out.println("Hello World!");
+//            }
+//        });
+//        
+//        StackPane root = new StackPane();
+//        root.getChildren().add(btn);
+//        primaryStage.setScene(new Scene(root, 300, 250));
+//        primaryStage.show();
+        
+        GridPane gridPane = new GridPane();
+        gridPane.setHgap(20);
+        gridPane.setVgap(10);
+        gridPane.setPadding(new Insets(10));
+//        ColumnConstraints c1 = new ColumnConstraints();
+//        c1.setPercentWidth(25);
+//        ColumnConstraints c2 = new ColumnConstraints();
+//        c2.setPercentWidth(25);
+//        ColumnConstraints c3 = new ColumnConstraints();
+//        c3.setPercentWidth(25);
+//        ColumnConstraints c4 = new ColumnConstraints();
+//        c4.setPercentWidth(25);
+//        gridPane.getColumnConstraints().addAll(c1, c2, c3, c4);
+		final ComboBox critterComboBox = new ComboBox();
+		critterComboBox.getItems().addAll(
+			"Craig",
+			"Algaephogic"
+		);
+        gridPane.add(new Label("Make Critter: "), 0, 0);
+        gridPane.add(critterComboBox, 0, 1);
+        gridPane.add(new Label("Amount"), 1, 1);
+        TextField critterAmount = new TextField();
+        gridPane.add(critterAmount, 2, 1);
+        Button critterButton = new Button();
+        critterButton.setText("Create");
+        critterButton.setOnAction(new EventHandler<ActionEvent>() {
+        	@Override
+        	public void handle(ActionEvent event) {
+        		try{
+            		System.out.println("Create Critter");
+            		int amount = Integer.parseInt(critterAmount.getText());
+            		System.out.println(amount);
+        		}
+        		catch (NumberFormatException e){
+    				System.out.println("error processing " + "'" + critterAmount.getText() + "'" + " in setting amount of Critters");
+    			}
+        	}
+        });
+        gridPane.add(critterButton, 3, 1);
+        
+        gridPane.add(new Label("Time Step:"), 0, 7);
+        Button singleTimeStepButton = new Button();
+        singleTimeStepButton.setText("Single");
+        singleTimeStepButton.setOnAction(new EventHandler<ActionEvent>(){
+        	@Override
+        	public void handle(ActionEvent event) {
+        		System.out.println("Single");
+        	}
+        });
+        gridPane.add(singleTimeStepButton, 0, 8);
+        gridPane.add(new Label("Amount"), 1, 8);
+        TextField timeStepAmount = new TextField();
+        gridPane.add(timeStepAmount, 2, 8);
+        Button multipleButton = new Button();
+        multipleButton.setText("Step");
+        multipleButton.setOnAction(new EventHandler<ActionEvent>(){
+        	@Override
+        	public void handle(ActionEvent event) {
+        		try{
+        			System.out.println("Multiple");
+            		int amount = Integer.parseInt(timeStepAmount.getText());
+            		System.out.println(amount);	
+        		}
+        		catch (NumberFormatException e){
+    				System.out.println("error processing " + "'" + timeStepAmount.getText() + "'" + " in setting amount of Time Steps");
+    			}
+        	}
+        });
+        gridPane.add(multipleButton, 3, 8);
+        
+        
+        gridPane.add(new Label("Set Seed:"), 0, 15);
+        gridPane.add(new Label("Amount"), 0, 16);
+        TextField seedAmount = new TextField();
+        gridPane.add(seedAmount, 1, 16);
+        Button seedButton = new Button();
+        seedButton.setText("Set");
+        seedButton.setOnAction(new EventHandler<ActionEvent>() {
+        	@Override
+        	public void handle(ActionEvent event) {
+        		try{
+        			System.out.println("Seed");
+            		int amount = Integer.parseInt(seedAmount.getText());
+            		System.out.println(amount);
+        		}
+        		catch (NumberFormatException e){
+    				System.out.println("error processing " +  "'" + seedAmount.getText() + "'" + " in setting amount for Seed");
+    			}
+        	}
+        });
+        gridPane.add(seedButton, 2, 16);
+       
+        gridPane.add(new Label("Animation:"), 0, 22);
+        gridPane.add(new Label("Speed"), 0, 23);
+        final ComboBox animationSpeedBox = new ComboBox();
+        animationSpeedBox.getItems().addAll(
+			"1x",
+			"2x"
+		);
+        gridPane.add(animationSpeedBox, 1, 23);
+        Button startButton = new Button();
+        startButton.setText("Start");
+        startButton.setOnAction(new EventHandler<ActionEvent>() {
+        	@Override
+        	public void handle(ActionEvent event) {
+        		System.out.println("Start");
+        	}
+        });
+        gridPane.add(startButton, 2, 23);
+        Button stopButton = new Button();
+        stopButton.setText("Stop");
+        stopButton.setOnAction(new EventHandler<ActionEvent>() {
+        	@Override
+        	public void handle(ActionEvent event) {
+        		System.out.println("Stop");
+        	}
+        });
+        gridPane.add(stopButton, 3, 23);
+        
+        primaryStage.setScene(new Scene(gridPane, 600, 450));
+        primaryStage.show();
+
 		
 	}
 
